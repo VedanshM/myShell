9155b3f4a3d022d8203d7a44b2475cf34c5d160c
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2020-09-04 22:19:01 +0530

    ls: total in -l

--- include/ls.h
+++ include/ls.h
@@ -3,7 +3,11 @@
 #include <dirent.h>
 
 int filter_hidden(const struct dirent *fil);
+size_t fileBlocks(char *fil);
+
 int ls(char **args, int argc);
 int ls_indv(char *fil);
 int ls_dir(char *dirpath);
+int ls_file(char *fileBase);
+
 #endif
--- src/ls.c
+++ src/ls.c
@@ -3,19 +3,15 @@
 #include "sysinfo.h"
 #include <dirent.h>
 #include <errno.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int l, a, echoDir;
-
-int filter_hidden(const struct dirent *fil) {
-	if (fil->d_name[0] == '.')
-		return 0;
-	return 1;
-}
 
 int ls(char **args, int argc) {
 	l = 0, a = 0;
@@ -57,15 +53,29 @@ int ls_indv(char *arg) {
 	char *path = process_path(arg, 0);
 	struct stat st;
 	stat(path, &st);
+	int ret = 0;
 	if (S_ISDIR(st.st_mode)) {
-		ls_dir(path);
+		ret = ls_dir(path);
+	} else {
+		ret = ls_file(path);
 	}
 	free(path);
-	return 0;
+	return ret;
 }
 
-// int ls_fil(char *fil, char *dir, int l, int a) {
-// }
+int ls_file(char *fil) { //can handle dirs too for -l compatibilty
+	char *tmp = realpath(fil, NULL);
+	if (tmp == NULL) {
+		fprintf(stderr, "Couldn't ls: %s", fil);
+		perror("error");
+		return -1;
+	}
+	if (l == 0) {
+		printf("%s ", basename(fil));
+	} else {
+	}
+	return 0;
+}
 
 int ls_dir(char *dirpath) {
 	if (echoDir)
@@ -84,12 +94,38 @@ int ls_dir(char *dirpath) {
 		perror("error:");
 		return -1;
 	}
+	char *fullpath = malloc(PATHMAX * sizeof(char));
+	strcat(strcpy(fullpath, dirpath), "/");
+	int n = strlen(fullpath);
 	if (l) {
+		size_t tot_blocks = 0;
+		for (int i = 0; i < nof; i++)
+			tot_blocks += fileBlocks(strcpy(fullpath + n, entries[i]->d_name) - n);
+		tot_blocks /= 2;
+		printf("total: %ld \n", tot_blocks);
+	}
 
-	} else {
-		for (int i = 0; i < nof; i++) {
-			printf("%s ", entries[i]->d_name);
-		}
+	for (int i = 0; i < nof; i++) {
+		ls_file(strcpy(fullpath + n, entries[i]->d_name) - n);
 	}
 	return 0;
+}
+
+int filter_hidden(const struct dirent *fil) {
+	if (fil->d_name[0] == '.')
+		return 0;
+	return 1;
+}
+
+size_t fileBlocks(char *fil) {
+	char *tmp = process_path(fil, 1);
+	struct stat st;
+	if (stat(tmp, &st) == -1) {
+		fprintf(stderr, "can't access : %s\n", fil);
+		perror("error");
+		free(tmp);
+		return 0;
+	}
+	free(tmp);
+	return st.st_blocks;
 }
