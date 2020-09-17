d1deff60a6b0c170a1c5346f9182c17cf3a96b62
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2020-09-17 15:36:12 +0530

    NOT WORKING

--- src/signals.c
+++ src/signals.c
@@ -1,7 +1,13 @@
 #include "signals.h"
 #include "bgprocess.h"
 #include <signal.h>
+#include <stdio.h>
 
 void init_sig_setup() {
-	signal(SIGCHLD, remove_bg);
+	struct sigaction sa;
+	sa.sa_handler = remove_bg;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		fprintf(stderr, "could not set signal for SIGCHLD ");
+		perror("error");
+	}
 }
